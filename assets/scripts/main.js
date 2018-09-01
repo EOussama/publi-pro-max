43eb936260dea86851e9c81852aa7a5cf55f0361
Eoussama <oussama_gtacj@live.fr>
2018-09-01 16:40:13 +0100

    Minor changes...
    * Change in README.md.
    * Spanish translation.
    * Logo changed.
    * Menu extended.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
     document.querySelector('.loadingPanel').style.opacity = '0';
     setTimeout(() => {
         document.querySelector('.loadingPanel').style.display = 'none';
@@ -6,72 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const
         navbarBurger = document.querySelector('.navbar-burger'),
-        menu = document.getElementById(navbarBurger.dataset.target),
-        langSelectEn = document.getElementById('langSelectEn'),
-        langSelectEs = document.getElementById('langSelectEs'),
-        langSelectFr = document.getElementById('langSelectFr'),
-        dict = {
-            "Home": {
-                'en': 'Home',
-                'es': 'Inicio',
-                'fr': 'Accueil'
-            },
-            "About": {
-                'en': 'About',
-                'es': 'A proposito',
-                'fr': 'À propos'
-            },
-            "Contact": {
-                'en': 'Contact',
-                'es': 'Contacto',
-                'fr': 'Contact'
-            },
-            "Language": {
-                'en': 'Language',
-                'es': 'Idioma',
-                'fr': 'Langue'
-            },
-            "English": {
-                'en': 'English',
-                'es': 'Inglés',
-                'fr': 'Anglais'
-            },
-            "Spanish": {
-                'en': 'Spanish',
-                'es': 'Español',
-                'fr': 'Espanol'
-            },
-            "French": {
-                'en': 'French',
-                'es': 'Francés',
-                'fr': 'Français'
-            },
-            "all in publi": {
-                'en': 'all in advertising',
-                'es': 'todos en publicidad',
-                'fr': 'tout dans la publicité'
-            },
-            "You have changed the display language.": {
-                'en': 'You have changed the display language.',
-                'es': 'Has cambiado el idioma de la pantalla.',
-                'fr': 'Vous avez modifié la langue d\'affichage.'
-            },
-        };
+        menu = document.getElementById(navbarBurger.dataset.target);
 
     navbarBurger.addEventListener('click', () => {
         navbarBurger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
-    });
-
-    langSelectEn.addEventListener('click', () => {
-        translate(document.body, dict, 'en');
-    });
-
-    langSelectEs.addEventListener('click', () => {
-        translate(document.body, dict, 'es');
-    });
-
-    langSelectFr.addEventListener('click', () => {
-        translate(document.body, dict, 'fr');
     });
 });
