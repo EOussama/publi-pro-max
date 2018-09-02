f305a9a46a35043d95450912c153e9a5738535cd
Eoussama <oussama_gtacj@live.fr>
2018-09-02 18:34:03 +0100

    Carousel added

---
+++ assets/scripts/carousel.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+    const carousel = document.getElementById('carousel');
+    var index = 1;
+
+    setInterval(() => {
+        if(index >= 3) {
+            index = 1;
+            carousel.style.top = '0';
+        } else
+            carousel.style.top = `-${window.innerHeight * index++}px`;
+    }, 5000);
+});
+
