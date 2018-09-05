64f0dc83fc8acc20cc147d63571fb719f7828782
Eoussama <oussama_gtacj@live.fr>
2018-09-05 16:00:55 +0100

    Minor design changes...
    * Carousel images blur toned down and opacity raised.
    * Scroll down button added.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,10 +6,19 @@ window.addEventListener('load', () => {
     
     const
         navbarBurger = document.querySelector('.navbar-burger'),
-        menu = document.getElementById(navbarBurger.dataset.target);
+        menu = document.getElementById(navbarBurger.dataset.target),
+        headerHeight = document.getElementsByTagName('header')[0],
+        scrollBtn = document.querySelector('.fa-chevron-down');
 
     navbarBurger.addEventListener('click', () => {
         navbarBurger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
+    });
+
+    scrollBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: headerHeight.clientHeight,
+            behavior: 'smooth'
+        });
     });
 });
