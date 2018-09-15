db9f3f982bf84b46c7dfe7b998667a557ae8a5b2
Eoussama <oussama_gtacj@live.fr>
2018-09-15 13:29:02 +0100

    Scrool to top button added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -38,8 +38,22 @@ window.addEventListener('load', () => {
         document.querySelector('.modal').classList.toggle('is-active');
     });
 
+    document.getElementById('scrollTopBtn').addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
     if(((new Date()).getFullYear()) > 2018)
         copyrightDate.textContent = `2018 - ${(new Date()).getFullYear()}`;
     else
         copyrightDate.textContent = 2018;
+});
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= document.getElementsByTagName('header')[1].offsetHeight)
+        document.getElementById('scrollTopBtn').style.display = 'block';
+    else
+        document.getElementById('scrollTopBtn').style.display = 'none';
 });
