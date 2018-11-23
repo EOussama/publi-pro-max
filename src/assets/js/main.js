fcc79d4d369fdc6d8d9ef75ec6dd29c9e4ef12c9
Eoussama <oussama_gtacj@live.fr>
2018-11-23 23:32:06 +0000

    Carousel and scrolling added.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(() => {
+    // #region Dropdowns
+
+    // Initializing the services dropdown.
     $('#services-dropdown').dropdown();
+
+    // Initializing the languages' dropdown.
+    $('#languages-dropdown').dropdown();
+    $('#languages-dropdown .menu .item:first-of-type').click();
+
+    // #endregion
+
+    // #region Carousel
+    $('#header-carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        draggable: false
+    });
+    // #endregion
+
+    // #region Scroll
+
+    $('i#scroll-down-btn').on('click', () => {
+        $('html').animate({
+            scrollTop: ($('nav').outerHeight() + $('header').outerHeight() + 2)
+        }, 500);
+    });
+
+    // #endregion
 });
